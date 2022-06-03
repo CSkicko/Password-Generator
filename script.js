a48a0778b266b8b781c1184b2a83468f00f559e8
Craig <craig.skicko@gmail.com>
2022-06-03 16:56:22 +1000

    Added arrays for character types

--- script.js
+++ script.js
@@ -23,6 +23,12 @@ var criteria = {
   }
 }
 
+// Set up arrays for characters
+var charLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var charUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var charNumeric = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var charSpecial = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
+
 // Initialise the message for the password length & types
 var lengthMessage = "Please select the desired legnth of the password by typing a value between 8 and 128";
 var typeMessage = "Type yes to include or any other value to omit."
